6e14872dbba2ecebb065c1f00e85ba8410be6904
rj1 <rj1@riseup.net>
2022-07-20 22:59:49 -0500

    updated email config

--- .config/email/check-email.sh
+++ .config/email/check-email.sh
@@ -1,15 +1,13 @@
 #!/bin/zsh
-source /home/rj1/.config/zsh/.zprofile
-
 # download mail from remotes
 if [ $# -eq -0 ]; then
-    mbsync -c $XDG_CONFIG_HOME/mbsync/config -a
+    /usr/bin/mbsync -c /home/rj1/.config/mbsync/config -aV
 else
-    mbsync -c $XDG_CONFIG_HOME/mbsync/config $1
+    /usr/bin/mbsync -c /home/rj1/.config/mbsync/config -V $1
 fi
 
 # add mail to notmuch db
-notmuch new
+/usr/bin/notmuch new
 
 # do some tagging
 notmuch tag +inbox -- folder:/inbox/ and tag:new
@@ -19,14 +17,14 @@ notmuch tag -new -- tag:new
 # notify
 SEARCH="tag:notify"
 
-NOTIFY_COUNT=$(notmuch count "$SEARCH")
+NOTIFY_COUNT=$(/usr/bin/notmuch count "$SEARCH")
 
 if [ "$NOTIFY_COUNT" -gt 0 ]; then
-  RESULTS=$(notmuch search --format=json --output=summary --limit=3 --sort="newest-first" "$SEARCH" | jq -r '.[] | "\(.authors): \(.subject)"')
+  RESULTS=$(/usr/bin/notmuch search --format=json --output=summary --limit=3 --sort="newest-first" "$SEARCH" | jq -r '.[] | "\(.authors): \(.subject)"')
   /usr/bin/notify-send "$NOTIFY_COUNT new mails:" "$RESULTS"
 fi
 
-notmuch tag -notify -- tag:notify
+/usr/bin/notmuch tag -notify -- tag:notify
 
 # refresh alot buffer if it's open
 pkill --signal SIGUSR1 alot
--- .config/email/oauth/oauth.sh
+++ .config/email/oauth/oauth.sh
@@ -1,4 +1,4 @@
-python /home/rj1/.config/mbsync/oauth/oauth2.py \
+python /home/rj1/.config/email/oauth/oauth2.py \
     --client_id="$(pass meta -a $1 'oauth_client_id')" \
     --client_secret="$(pass meta -a $1 'oauth_client_secret')" \
     --refresh_token="$(pass meta -a $1 'oauth_refresh_token')" \
