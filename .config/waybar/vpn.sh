6c7e070ba07a72ac978a787445f04a6f5bd7519d
rj1 <rj1@riseup.net>
2022-09-16 01:45:18 -0500

    waybar: no need to launch on specific displays anymore, update vpn indicator

--- .config/waybar/vpn.sh
+++ .config/waybar/vpn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ip route | grep -q '10.6.0.2' \
+ip route | grep -q '10.6.0.5' \
 && echo '{"text":"Connected","class":"connected","percentage":100}' \
 || echo '{"text":"Disconnected","class":"disconnected","percentage":0}'
 
