6a6017df1d753a6c4bb1b107cc549d3c2ca83a0e
rj1 <rj1@riseup.net>
2022-11-08 00:28:10 -0600

    replaced waybar with yambar

--- .config/waybar/vpn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ip route | grep -q '10.6.0.5' \
-&& echo '{"text":"Connected","class":"connected","percentage":100}' \
-|| echo '{"text":"Disconnected","class":"disconnected","percentage":0}'
-
