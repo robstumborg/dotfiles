2f98d9a2ac226894161730a334d471db65342e74
rj1 <rj1@riseup.net>
2023-03-01 17:28:04 -0600

    sway: changed terminal launch script

--- .config/sway/scripts/alacritty.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ `pgrep alacritty` != 0 ]
-then
-    alacritty
-else
-    alacritty msg create-window
-fi;
-
--- .config/sway/scripts/foot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $(pgrep foot) != 0 ]; then
-  foot -s
-  footclient
-else
-  footclient
-fi
