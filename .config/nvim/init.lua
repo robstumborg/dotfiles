0a2a3e7ca9cf2995f4fb8ebf07594cbd3dd9a67c
rj1 <rj1@riseup.net>
2023-03-14 13:56:08 -0600

    nvim: wiki -> notes, changed :Daily for :Today + :Yesterday

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -165,10 +165,10 @@ vim.api.nvim_create_autocmd("filetype", {
 -- vimux
 vim.g.vimuxheight = 32
 
--- wiki
-local wiki_dir = vim.fn.expand("$HOME/wiki")
+-- notes
+local notes_dir = vim.fn.expand("$HOME/notes")
 vim.api.nvim_create_autocmd({ "bufenter", "bufleave" }, {
-	pattern = { wiki_dir .. "/**/*.md" },
+	pattern = { notes_dir .. "/**/*.md" },
 	command = "setl noswapfile noundofile nobackup viminfo=",
 })
 
@@ -187,18 +187,18 @@ require("mkdnflow").setup({
 
 local function daily_file()
 	local date_string = os.date("%Y-%m-%d")
-	local dir = vim.fn.expand(wiki_dir .. "/daily/")
+	local dir = vim.fn.expand(notes_dir .. "/daily/")
 	return dir .. date_string .. ".md"
 end
 
 -- create daily journal entry command
-vim.api.nvim_create_user_command("Daily", function()
+vim.api.nvim_create_user_command("Today", function()
 	local file_path = daily_file()
 	local f = io.open(file_path, "r")
 	if f ~= nil then
 		f:close()
 	else
-		local dir = vim.fn.expand(wiki_dir .. "/daily/")
+		local dir = vim.fn.expand(notes_dir .. "/daily/")
 		local template = io.open(dir .. "template.md", "r")
 		if template == nil then
 			vim.cmd("echo 'no daily template found in " .. dir .. "'")
@@ -214,6 +214,16 @@ vim.api.nvim_create_user_command("Daily", function()
 		end
 	end
 	vim.cmd("edit " .. file_path)
+end, {})
+
+vim.api.nvim_create_user_command("Yesterday", function()
+	local date_string = os.date("%Y-%m-%d", os.time() - 86400)
+	local file_path = vim.fn.expand(notes_dir .. "/daily/" .. date_string .. ".md")
+	if vim.fn.filereadable(file_path) == 1 then
+		vim.cmd("edit " .. file_path)
+	else
+		vim.cmd("echo 'no daily entry for " .. date_string .. "'")
+	end
 end, {})
 
 local function daily_status()
