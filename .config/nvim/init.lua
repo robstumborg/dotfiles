4b5d931556118110689a3e030db88cab98f9c99f
rj1 <rj1@riseup.net>
2023-04-18 10:46:24 -0600

    nvim: fix nvim-tree config as per https://github.com/nvim-tree/nvim-tree.lua/wiki/Migrating-To-on_attach

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -720,13 +720,15 @@ require("diffview").setup({})
 
 -- nvim-tree
 require("nvim-tree").setup({
+	on_attach = function(bufnr)
+		local api = require("nvim-tree.api")
+		api.config.mappings.default_on_attach(bufnr)
+		vim.keymap.set("n", "s", "", { buffer = bufnr })
+		vim.keymap.del("n", "s", { buffer = bufnr })
+	end,
+
 	view = {
 		relativenumber = true,
-		mappings = {
-			list = {
-				{ key = "s", action = "" },
-			},
-		},
 	},
 })
 vim.cmd("command! Diff DiffviewOpen")
