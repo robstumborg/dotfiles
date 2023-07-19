4ea5f92e15d3eaeb310112b518ecd3c72a6eb660
Rob Stumborg <rob@stumb.org>
2023-07-18 18:33:12 -0600

    yambar: added custom modules, some tweaks

--- .config/yambar/task.sh
+++ .config/yambar/task.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-task=$(dstask show-active | jq -r 'if length == 0 then "none" else .[0] | "\(.summary) (id: \(.id))" end')
+task=$(dstask show-active | jq -r 'if length == 0 then "task: none" else .[0] | "task \(.id): \(.summary)" end')
 echo "task|string|${task}"
 echo ""
