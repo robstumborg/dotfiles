c4fc7b380c1f4412dd27e3e7cfe474e6bed15265
Rob Stumborg <rob@stumb.org>
2023-09-10 20:18:01 -0600

    nvim: js snippets for ft=typescript

--- .config/nvim/lua/rj1/lsp.lua
+++ .config/nvim/lua/rj1/lsp.lua
@@ -296,6 +296,7 @@ local kind_icons = {
 
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip").filetype_extend("javascript", { "javascriptreact" })
+require("luasnip").filetype_extend("typescript", { "javascript" })
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 cmp.setup({
