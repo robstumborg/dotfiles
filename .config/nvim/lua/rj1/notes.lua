7207d3d5831ad9422cf414305d066af949c9b05e
Rob Stumborg <rob@stumb.org>
2024-03-22 08:04:53 -0600

    nvim: added WorkTomorrow command for notes - notes in general could use a refactor

--- .config/nvim/lua/rj1/notes.lua
+++ .config/nvim/lua/rj1/notes.lua
@@ -86,6 +86,43 @@ function Daily_status(directory)
 	end
 end
 
+-- function to get file path for tomorrow's file
+local function tomorrow_file(directory, template_path)
+	local date_string = os.date("%Y-%m-%d", os.time() + 86400)
+	local dir = vim.fn.expand(directory .. "/daily/")
+	return dir .. date_string .. ".md"
+end
+
+-- create or open file for tomorrow's entry
+local function create_or_open_tomorrow_file(directory, template_path)
+	local file_path = tomorrow_file(directory, template_path)
+	local f = io.open(file_path, "r")
+	if f ~= nil then
+		f:close()
+	else
+		local dir = vim.fn.expand(directory .. "/daily/")
+		local template = io.open(dir .. template_path, "r")
+		if template == nil then
+			vim.cmd("echo 'no daily template found in " .. dir .. "'")
+			return
+		end
+		local template_content = template:read("*all")
+		template:close()
+		local new_file = io.open(file_path, "w")
+		if new_file then
+			new_file:write("# " .. os.date("%Y-%m-%d", os.time() + 86400) .. "\n\n")
+			new_file:write(template_content)
+			new_file:close()
+		end
+	end
+	vim.cmd("edit " .. file_path)
+end
+
+-- create WorkTomorrow command
+vim.api.nvim_create_user_command("WorkTomorrow", function()
+	create_or_open_tomorrow_file(work_notes_dir, "template.md")
+end, {})
+
 vim.api.nvim_create_user_command("QuickNote", function(opts)
 	local note = opts.args or nil
 	local file_path = vim.fn.expand(notes_dir .. "/quick-notes.md")
